0add5fbeb37c55a22c7fff33fc3b8c042c55d704
Gihanvs98 <darkcreationsl98@gmail.com>
2025-11-16 19:51:58 +0530

    feat(supplier):created id for each UI elements

--- src/com/pcl/inventory/controller/SupplierManagementFormController.java
+++ src/com/pcl/inventory/controller/SupplierManagementFormController.java
@@ -1,4 +1,33 @@
 package com.pcl.inventory.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class SupplierManagementFormController {
+    public TextField txtSearch;
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtContactPerson;
+    public TextField txtContactNumber;
+    public TextField txtEmail;
+    public TextField txtAddress1;
+    public TextField txtAddress2;
+    public TextField txtCity;
+    public TextField txtCountry;
+    public TableView tblSupplier;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colContactPerson;
+    public TableColumn colPhone;
+    public TableColumn colEmail;
+    public TableColumn colAddress1;
+    public TableColumn colAddress2;
+    public TableColumn colCity;
+    public TableColumn colCountry;
+    public TableColumn colAction;
+
+    public void saveOnAction(ActionEvent actionEvent) {
+    }
 }
